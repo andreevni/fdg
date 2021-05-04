6b72ea3843b31847eab237998f5baeef2c67c0ea
andreevni <nickita956@gmail.com>
2021-05-04 21:46:45 +0300

    change PersonTest

---
+++ src/test/java/ruItsJava/collections/lists/PersonTest.java
@@ -0,0 +1,40 @@
+package ruItsJava.collections.lists;
+
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.itsjava.git.Person;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class PersonTest {
+    @DisplayName("Check Get Method : ")
+    @Test
+    public void checkGetMethod() {
+        Person actualPerson = new Person("Vanya", 21);
+        assertAll("tryGetters", () -> assertEquals("Vanya", actualPerson.getName()),
+                () -> assertEquals(21, actualPerson.getAge()));
+    }
+
+    @DisplayName("check correct work of method takeBeer() : ")
+    @Test
+    public void shouldCheckAgeForSellBeer() {
+        Person actualPerson1 = new Person("Vova", 19);
+        Person actualPerson2 = new Person("Vitya", 15);
+
+        assertAll("differentPersonAges", () -> assertEquals(actualPerson1.takeBeer(), true),
+                () -> assertEquals(actualPerson2.takeBeer(), false));
+    }
+
+    @DisplayName(" should add age :")
+    @Test
+    public void shouldAddAge() {
+        Person actualPerson1 = new Person("Vova", 19);
+        actualPerson1.birthday();
+        assertEquals(actualPerson1.getAge(), 20);
+    }
+
+}
