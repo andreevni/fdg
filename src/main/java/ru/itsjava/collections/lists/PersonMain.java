27dfad2cb189a1a898b78a96dccd0c68b96c5fde
andreevni <nickita956@gmail.com>
2021-05-08 00:07:25 +0300

    07.05.2021 home work

--- src/main/java/ru/itsjava/collections/lists/PersonMain.java
+++ src/main/java/ru/itsjava/collections/lists/PersonMain.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class PersonMain {
     public static void main(String[] args) {
         Person vasiliy = new Person("Vasiliy", false, 18);
-        Person tom = new Person("Harry", false, 21);
+        Person harry = new Person("Harry", false, 21);
         Person henri = new Person("Henri", false, 25);
         Person eerki = new Person("Eerki", false, 24);
         Person ilya = new Person("Ilya", false, 31);
@@ -16,7 +16,7 @@ public class PersonMain {
         Person nastya = new Person("Nastya", true, 25);
         Person anna = new Person("Anna", true, 29);
 
-        List<Person> listPerson = new ArrayList<>(Arrays.asList(vasiliy, tom, henri, eerki, ilya, olga, svetlana, nastya, anna));
+        List<Person> listPerson = new ArrayList<>(Arrays.asList(vasiliy, harry, henri, eerki, ilya, olga, svetlana, nastya, anna));
 // Возвращаем всех военнообязанных мужчин, которым меньше 27 и больше 18 и имя которых начинается на Н
         for (int i = 0; i < listPerson.size(); i++) {
             if (listPerson.get(i).isMale() == false && listPerson.get(i).getAge() < 27 && listPerson.get(i).getAge() > 18 && listPerson.get(i).getName().contains("H")) {
