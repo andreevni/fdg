dbb27266b74e3846e339cc83b77110f3aebf0e2b
andreevni <nickita956@gmail.com>
2021-05-17 21:52:26 +0300

    LinkedListTest for MyLinkedList.java HomeWork

---
+++ src/test/java/ruItsJava/collections/lists/MyLinkedListTest.java
@@ -0,0 +1,111 @@
+package ruItsJava.collections.lists;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.itsjava.collections.lists.linkedlist.MyLinkedLIst;
+
+@DisplayName("Class MyLinkedList")
+public class MyLinkedListTest {
+    @DisplayName("Check size : ")
+    @Test
+    public void shouldHaveCorrectSizeMethod() {
+        MyLinkedLIst linkedList = new MyLinkedLIst();
+        linkedList.add("String1");
+        linkedList.add("String2");
+        Assertions.assertEquals(linkedList.size(), 2);
+    }
+
+    @DisplayName("Check Add method : ")
+    @Test
+    public void shouldHaveCorrectAdd() {
+        MyLinkedLIst linkedList = new MyLinkedLIst();
+        linkedList.add("String1");
+        linkedList.add("String2");
+        Assertions.assertEquals(linkedList.size(), 2);
+
+    }
+
+    @DisplayName("Check how to work get() : ")
+    @Test
+    public void shouldHaveGetMethod() {
+        MyLinkedLIst linkedList = new MyLinkedLIst();
+        linkedList.add("String1");
+        linkedList.add("String2");
+        linkedList.add("String3");
+        linkedList.add("String4");
+        Assertions.assertEquals(linkedList.get(0), "String1");
+        Assertions.assertEquals(linkedList.get(1), "String2");
+        Assertions.assertEquals(linkedList.get(3), "String4");
+        Assertions.assertEquals(linkedList.get(4), null);
+    }
+
+    @DisplayName("Check clear method : ")
+    @Test
+    public void shouldHaveCorrectClearMethod() {
+        MyLinkedLIst linkedList = new MyLinkedLIst();
+        linkedList.add("String1");
+        linkedList.add("String2");
+        linkedList.add("String3");
+        linkedList.add("String4");
+        linkedList.clear();
+        Assertions.assertEquals(linkedList.size(), 0);
+    }
+
+    @DisplayName("Check IsEmpty method : ")
+    @Test
+    public void shouldHaveCorrectIsEmptyMethod() {
+        MyLinkedLIst linkedList = new MyLinkedLIst();
+        linkedList.add("String1");
+        linkedList.add("String2");
+        Assertions.assertEquals(linkedList.isEmpty(), false);
+        linkedList.clear();
+        Assertions.assertEquals(linkedList.isEmpty(), true);
+    }
+
+    @DisplayName("Check how work contains() : ")
+    @Test
+    public void shouldHaveCorrectContainsMethod() {
+        MyLinkedLIst linkedList = new MyLinkedLIst();
+        linkedList.add("String1");
+        linkedList.add("String2");
+        linkedList.add("String3");
+        linkedList.add("String4");
+        Assertions.assertTrue(linkedList.contains("String1"));
+        Assertions.assertTrue(linkedList.contains("String3"));
+        Assertions.assertTrue(linkedList.contains("String4"));
+        Assertions.assertFalse(linkedList.contains("String25"));
+    }
+
+    @DisplayName("Check how work remove() : ")
+    @Test
+    public void shouldHaveCorrectRemove() {
+        MyLinkedLIst linkedList = new MyLinkedLIst();
+        linkedList.add("String1");
+        linkedList.add("String2");
+        linkedList.add("String3");
+        linkedList.add("String4");
+        linkedList.remove("String1");
+        linkedList.remove("String4");
+        linkedList.remove("String3");
+        Assertions.assertEquals(linkedList.size(), 1);
+    }
+
+    @DisplayName("Check how work set() : ")
+    @Test
+    public void shouldHaveCorrectSetMethod() {
+        MyLinkedLIst linkedList = new MyLinkedLIst();
+        linkedList.add("String1");
+        linkedList.add("String2");
+        linkedList.add("String3");
+        linkedList.add("String4");
+        linkedList.set(0, "String21");
+        linkedList.set(2, "String22");
+        linkedList.set(3, "String23");
+        Assertions.assertEquals(linkedList.get(0), "String21");
+        Assertions.assertEquals(linkedList.get(2), "String22");
+        Assertions.assertEquals(linkedList.get(3), "String23");
+    }
+
+
+}
