3adf51c5f3654e6e003fd06a8bdd61e9808e7500
andreevni <nickita956@gmail.com>
2021-05-17 21:50:31 +0300

    LinkedList HomeWork

--- src/main/java/ru/itsjava/collections/lists/linkedlist/MyLinkedLIst.java
+++ src/main/java/ru/itsjava/collections/lists/linkedlist/MyLinkedLIst.java
@@ -5,15 +5,20 @@ public class MyLinkedLIst {
     private Node head;
 
     public int size() {
-        Node resNode = new Node(head,head.getNext());
+        Node curNode = head;
         int count = 0;
-        while (resNode.getNext() != null){
+        if (head == null) {
+            return 0;
+        } else {
             count++;
-            resNode = resNode.getNext();
-
         }
 
-        return 0;
+        while (curNode.getNext() != null) {
+            count++;
+            curNode = curNode.getNext();
+
+        }
+        return count;
     }
 
     public boolean isEmpty() {
@@ -24,6 +29,18 @@ public class MyLinkedLIst {
     }
 
     public boolean contains(Object o) {
+        Node curNode = head;
+        if (head == null) {
+            return false;
+        } else if (o == head.getValue()) {
+            return true;
+        }
+        while (curNode.getNext() != null) {
+            curNode = curNode.getNext();
+            if (curNode.getValue() == o) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -44,35 +61,155 @@ public class MyLinkedLIst {
     }
 
     public boolean remove(Object o) {
+        Node prevNode = head;
+        Node curNode = head;
+        if (head == null) return false;
+        if (head.getValue().equals(o)) {
+            head = head.getNext();
+            return true;
+        }
+        if (head.getNext() == null) {
+            return false;
+        }
+        while ((curNode = curNode.getNext()) != null) {
+            if (curNode.getValue().equals(o)) {
+                break;
+            }
+            prevNode = prevNode.getNext();
+        }
+        if (curNode == null) {
+            return false;
+        }
+        prevNode.setNext(curNode.getNext());
+        curNode.setNext(null);
         return false;
     }
 
     public void clear() {
+        head = null;
 
     }
 
     public Object get(int index) {
+        Node curNode = head;
+        int count = 0;
+        if (index == 0) {
+            return head.getValue();
+        }
+        while ((curNode = curNode.getNext()) != null) {
+            count++;
+            if (count == index) {
+                return curNode.getValue();
+            }
+        }
         return null;
     }
 
+
     public Object set(int index, Object element) {
+        Node curNode = head;
+        checkIndex(index);
+        int count = 0;
+        if (head != null && index == 0) {
+            head.setValue(element);
+            return head.getValue();
+        }
+        while (curNode.getNext() != null) {
+            count++;
+            curNode = curNode.getNext();
+            if (count == index) {
+                curNode.setValue(element);
+                return curNode.getValue();
+            }
+        }
         return null;
     }
 
     public void add(int index, Object element) {
+        Node resNode = head;
+        Node curNode = head;
+        int count = 0;
+        if (head != null && index == 0) {
+            head = head.getNext();
+        }
+
 
     }
 
     public Object remove(int index) {
-        return null;
+        checkIndex(index);
+        if (index == 0) {
+            Object resValue = head.getValue();
+            if (head.getNext() == null) {
+                head = null;
+            } else {
+                head = head.getNext();
+            }
+            return resValue;
+
+        }
+        Node curNode = head;
+        Node prevNode = head;
+        int count = 0;
+        while ((curNode = curNode.getNext()) != null) {
+            count++;
+            if (count == index) {
+                break;
+            }
+            prevNode = prevNode.getNext();
+        }
+        if (curNode == null) return false;
+        Object resValue = curNode.getValue();
+        if (curNode.getNext() == null) {
+            prevNode.setNext(null);
+        } else {
+            prevNode.setNext(curNode.getNext());
+            curNode.setNext(null);
+        }
+        return resValue;
+    }
+
+    private void checkIndex(int index) {
+        if (!isCorrectIndex(index)) {
+            throw new ArrayIndexOutOfBoundsException("Некорректный индекс");
+        }
+    }
+
+    private boolean isCorrectIndex(int index) {
+        if ((index >= 0) && (index <= size())) {
+            return true;
+        }
+        return false;
     }
 
     public int indexOf(Object o) {
-        return 0;
+        Node curNode = head;
+        int count = 0;
+        if (head != null && head.getValue().equals(o)) {
+            return count;
+        }
+
+        while (curNode.getNext() != null) {
+            count++;
+            curNode = curNode.getNext();
+            if (curNode.getValue().equals(o)) {
+                return count;
+            }
+        }
+        return -1;
     }
 
     public int lastIndexOf(Object o) {
-        return 0;
+        Node curNode = head;
+        Node resNode = head;
+        int count = 0;
+        int count2 = 0;
+        if (head.getValue().equals(o)) {
+            return 0;
+        }
+
+
+        return -1;
     }
 
     @Override
--- src/main/java/ru/itsjava/collections/lists/linkedlist/MyLinkedListPrac.java
+++ src/main/java/ru/itsjava/collections/lists/linkedlist/MyLinkedListPrac.java
@@ -2,13 +2,30 @@ package ru.itsjava.collections.lists.linkedlist;
 
 public class MyLinkedListPrac {
     public static void main(String[] args) {
-     MyLinkedLIst list = new MyLinkedLIst();
-     list.add("String1");
+        MyLinkedLIst list = new MyLinkedLIst();
+        list.add("String1");
         System.out.println("list = " + list);
         list.add("String2");
         System.out.println(list);
         list.add("String3");
+        list.add("String4");
+
+//        System.out.println(list);
+//        System.out.println("list.size() = " + list.size());
+//
+//        System.out.println(list);
+//        System.out.println("list.size() = " + list.size());
+
+
+//        System.out.println("get : " + list.get(3));
+//        System.out.println("list : "+list);
+//        System.out.println("list.contains(\"String2\") = " + list.contains("String2"));
+//        System.out.println("list.indexOf(\"S\") = " + list.indexOf("String4"));
+        list.add("String2");
+
         System.out.println(list);
-        System.out.println("list.size() = " + list.size());
+        System.out.println("list.lastIndexOf(\"String1\") = " + list.lastIndexOf("String2"));
+
+
     }
 }
